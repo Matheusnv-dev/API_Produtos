175e22629aca087530bd411e2ee2cf75f567c45d
Programador Full Stack 36133232025.1 <matheusnvy0@gmail.com>
2025-07-09 21:29:23 -0300

    Subindo primeira versão

--- api/src/main/java/com/apiproduto/api/controller/ProdutoControler.java
+++ api/src/main/java/com/apiproduto/api/controller/ProdutoControler.java
@@ -1,9 +1,14 @@
 package com.apiproduto.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.apiproduto.api.models.Produto;
 import com.apiproduto.api.models.ProdutoRepository;
 
 
@@ -14,13 +19,21 @@ public class ProdutoControler {
     private ProdutoRepository repository;
 
      @GetMapping("/listagem") // muda a url
-    public String listarProdutos(){
+    public String listarProdutos(Model model){
+     model.addAttribute("todosOsProdutos", repository.findAll());
         return "listar";
     }
     @GetMapping("/Cadastro") 
-    public String cadastrarProdutos(){
+    public String mostrarProdutos(Model model){
+        model.addAttribute("produto", new Produto());
         return "Cadastrar";
     }
-    
+    @PostMapping("/Cadastro")
+
+    public String cadastrarProdutos(Produto produto){
+       repository.save(produto);
+
+    return "cadastrar";
+}
 }
 
